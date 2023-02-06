c03f7fe8dfcc6b4a97dec3bd47c1cc92b1309080
Juan Felipe Rada <j.f.r.a1997@gmail.com>
2023-02-06 09:15:59 -0300

    feat: Create base projects and add Assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+/*
+ * RADAPLS PROJECTS
+ * ------------------
+ * Copyright (C) 2023 Juan Felipe Rada - All Rights Reserved.
+ *
+ * This file, project or its parts can not be copied and/or distributed without
+ * the express permission of Juan Felipe Rada.
+ *
+ * @file App.jsx
+ * @author Juan Felipe Rada <radapls8@gmail.com>
+ * @date Monday, 6th February 2023
+ */
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div >
+        Desde el app
     </div>
   )
 }
